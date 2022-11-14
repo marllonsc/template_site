02c07549f062c00db7ed4a5298385e5261d32a8e
marllonsc <marllonsc@gmail.com>
2022-11-14 17:31:54 -0300

    next step working on background image

--- aje_site/src/app/app.module.ts
+++ aje_site/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BtntalkComponent } from './btntalk/btntalk.component';
+import { BannerComponent } from './banner/banner.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BtntalkComponent
+    BtntalkComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
